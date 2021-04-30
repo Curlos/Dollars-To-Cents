0e998fa8e4912e0c809789079ec12668f2c98c10
Carlos Martinez <curlosmart@gmail.com>
2021-04-29 20:49:11 -0400

    total cents from the converted dollar value

--- script.js
+++ script.js
@@ -2,6 +2,39 @@ document.getElementById('convert-to-cents').addEventListener('click', () => {
     let dollars = Number(document.getElementById('dollars').value.trim());
 
 
-    convertDollarsToCents(dollars);
-    console.log(dollars);
+    convertDollarsToCents(Math.round(dollars * 100));
 })
+
+const convertDollarsToCents = (dollars) => {
+    document.getElementById('totalCents').innerHTML = 'Total Cents: ' + dollars;
+
+    let coins = {
+        25: 0,
+        10: 0,
+        5: 0,
+        1: 0,
+    }
+
+    let coinsArr = Object.keys(coins)
+    coinsArr.reverse()
+
+    let i = 0
+
+    while (dollars > 0) {
+        console.log(dollars);
+        i++;
+        for (let key of coinsArr) {
+            let coin = Number(key);
+
+            if(dollars - coin >= 0) {
+                dollars = Math.round(dollars - coin);
+                coins[key] += 1
+                break;
+            }
+        }
+    }
+
+
+    console.log(Object.values(coins));
+    console.log(Object.entries(coins));
+}
